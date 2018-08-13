04c1889e1c40fd871314df9915878cdbd35c0640
ahkhystrix <ahk.hsytrix@gmail.com>
2018-08-13 23:19:15 +0300

    web-forms on flask has been added

--- randomuser.py
+++ randomuser.py
@@ -1,4 +1,22 @@
 import requests
+from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import SubmitField
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "My first security key"
+
+
+class GetRandomUsersForm(Form):
+    submit = SubmitField("Get random users")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    form = GetRandomUsersForm()
+    if form.validate_on_submit():
+        users = get_randomuser(10)
+        return render_template("list.html", users=users)
+    return render_template("index.html", form=form)
 
 
 def get_randomuser(count):
@@ -10,5 +28,4 @@ def get_randomuser(count):
 
 
 if __name__ == "__main__":
-    users = get_randomuser(2)
-    print(users)
+    app.run(debug=True)
